d6fbe106b97f11947edcf7aa8dc43106a5284c72
mzxdream <mzxdream@gmail.com>
2016-07-05 10:31:58 +0800

    1

--- src/util/m_errno.cpp
+++ src/util/m_errno.cpp
@@ -67,50 +67,50 @@ std::string MGetErrorMsg(MError err)
     case MError::OperationNotPermitted:
         return "operation not permitted";
     case MError::NoSuchFileOrDirectory:
-        return "NoSuchFileOrDirectory";
+        return "no such file or directory";
     case MError::NoSuchProcess:
-        return "NoSuchProcess";
+        return "no such process";
     case MError::InterruptedSystemCall:
-        return "InterruptedSystemCall";
+        return "interrupted system call";
     case MError::IOError:
-        return "IOError";
-    case ENXIO:
-        return MError::NoSuchDeviceOrAddress;
-    case E2BIG:
-        return MError::ArgumentListTooLong;
-    case ENOEXEC:
-        return MError::ExecFormatError;
-    case EBADF:
-        return MError::BadFileNumber;
-    case ECHILD:
-        return MError::NoChildProcesses;
-    case EAGAIN:
-        return MError::TryAgain;
-    case ENOMEM:
-        return MError::OutOfMemory;
-    case EACCES:
-        return MError::PermissionDenied;
-    case EBUSY:
-        return MError::DeviceOrResourceBusy;
-    case EINVAL:
-        return MError::InvalidArgument;
-    case ENFILE:
-        return MError::FileTableOverflow;
-    case EMFILE:
-        return MError::TooManyOpenFiles;
-    case EWOULDBLOCK:
-        return MError::TryAgain;
-    case ENODATA:
-        return MError::NoData;
-    case ETIME:
-        return MError::TimerExpired;
-    case EADDRINUSE:
-        return MError::AddressInUse;
-    case ENOBUFS:
-        return MError::NoBufferSpace;
-    case ECONNREFUSED:
-        return MError::ConnectionRefused;
+        return "io error";
+    case MError::NoSuchDeviceOrAddress:
+        return "no such device or address";
+    case MError::ArgumentListTooLong:
+        return "argument list too long";
+    case MError::ExecFormatError:
+        return "exec format error";
+    case MError::BadFileNumber:
+        return "bad file number";
+    case MError::NoChildProcesses:
+        return "no child processes";
+    case MError::TryAgain:
+        return "try again";
+    case MError::OutOfMemory:
+        return "out of memory";
+    case MError::PermissionDenied:
+        return "permission denied";
+    case MError::DeviceOrResourceBusy:
+        return "device or resource busy";
+    case MError::InvalidArgument:
+        return "invalid argument";
+    case MError::FileTableOverflow:
+        return "file table overflow";
+    case MError::TooManyOpenFiles:
+        return "too many open files";
+    case MError::TryAgain:
+        return "try again";
+    case MError::NoData:
+        return "no data";
+    case MError::TimerExpired:
+        return "timer expired";
+    case MError::AddressInUse:
+        return "address in use";
+    case MError::NoBufferSpace:
+        return "no buffer space";
+    case MError::ConnectionRefused:
+        return "connection refused";
     default:
-        return MError::Unknown;
+        return "unknown error";
     }
 }
