7a75377d79f4b75674ffca7e360da2329f411691
mzxdream <mzxdream@gmail.com>
2018-01-13 17:22:27 +0800

    1

--- include/mzx/ecs/entity_manager.h
+++ include/mzx/ecs/entity_manager.h
@@ -1,6 +1,10 @@
 #ifndef __MZX_ECS_ENTITY_MANAGER_H__
 #define __MZX_ECS_ENTITY_MANAGER_H__
 
+#include <functional>
+
+#include <mzx/ecs/entity.h>
+
 namespace mzx {
 namespace ecs {
 
@@ -9,7 +13,29 @@ class EntityManager
 public:
     EntityManager();
     ~EntityManager();
+    EntityManager(const EntityManager &) = delete;
+    EntityManager & operator=(const EntityManager &) = delete;
 public:
+    Entity * CreateEntity();
+    void DestroyEntity(Entity::IDType id);
+    void DestroyAllEntity();
+    Entity * GetEntity(Entity::IDType id);
+    template <typename T, typename ...Args>
+    void ForeachEntity(const std::function<void (Entity *)> &cb)
+    {
+        for (auto &iter_entity : entity_list_)
+        {
+            if (!iter_entity.second->HasComponent<T, Args...>())
+            {
+                continue;
+            }
+            cb(iter_entity.second);
+        }
+    }
+    void ForeachAllEntity(const std::function<void (Entity *)> &cb);
+private:
+    Entity::IDType next_entity_id_;
+    std::map<Entity::IDType, Entity *> entity_list_;
 };
 
 }
