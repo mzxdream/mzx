2e695b27627b3cee9e5c5ecacbde7a97b7ba021e
mzxdream <mzxdream@gmail.com>
2018-06-25 21:22:17 +0800

    1

--- include/mzx/unicode_util.h
+++ include/mzx/unicode_util.h
@@ -5,7 +5,7 @@
 
 namespace mzx {
 
-void ForeachUtf8Char(const char *utf8str, std::function<bool (const char *, std::size_t len)> cb);
+void ForeachUtf8Char(const char *utf8str, std::function<bool (const char * str, std::size_t len)> cb);
 bool IsChineseUtf8Char(const char *str, std::size_t len);
 bool IsEnglishUtf8Char(const char *str, std::size_t len);
 bool IsNumberUtf8Char(const char *str, std::size_t len);
