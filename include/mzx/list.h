d2885823b1d8da5e823b01fa71f21ed310086321
mzxdream <mzxdream@gmail.com>
2018-12-15 22:38:34 +0800

    add list

--- include/mzx/list.h
+++ include/mzx/list.h
@@ -5,15 +5,15 @@ namespace mzx {
 
 struct ListHead
 {
-    ListHead *prev{ nullptr };
-    ListHead *next{ nullptr };
+    ListHead *prev;
+    ListHead *next;
 };
 
 #define MZX_LIST_HEAD_INIT(name) \
-    { &(name), &(name) }
+    { .prev = &(name), .next = &(name) }
 
 #define MZX_LIST_HEAD(name) \
-    ListHead name = LIST_HEAD_INIT(name)
+    mzx::ListHead name = MZX_LIST_HEAD_INIT(name)
 
 #define MZX_INIT_LIST_HEAD(head) \
     do {\
@@ -21,26 +21,43 @@ struct ListHead
         (head)->next = (head);\
     } while (0)
 
-#define MZX_LIST_INSERT(node, prev, next) \
+#define MZX_LIST_INSERT(node, _pre, _nxt) \
     do {\
-        (next)->prev = (node);\
-        (node)->next = (next);\
-        (node)->prev = (prev);\
-        (prev)->next = (node);\
+        auto prev = (_pre);\
+        auto next = (_nxt);\
+        next->prev = (node);\
+        (node)->next = next;\
+        (node)->prev = prev;\
+        prev->next = (node);\
     } while (0)
 
 #define MZX_LIST_PUSH_FRONT(node, head) \
-    MZX_LIST_INSERT(node, head, head->next)
+    MZX_LIST_INSERT(node, head, (head)->next)
 
 #define MZX_LIST_PUSH_BACK(node, head) \
-    MZX_LIST_INSERT(node, head->prev, head)
+    MZX_LIST_INSERT(node, (head)->prev, head)
 
-#define MZX_LIST_ENTRY(ptr, type, member) \
+#define MZX_LIST_ENTRY(node, type, member) \
     ({\
-        const typeof(((type *)0)->member) *mptr = (ptr);\
-        (type *)((char *)mptr - (std::size_t) &((type *)0)->member);\
+        const decltype(((type *)0)->member) *ptr = (node);\
+        (type *)((char *)ptr - (std::size_t) &((type *)0)->member);\
     })
 
+#define MZX_LIST_REMOVE_UNINIT(node) \
+    do {\
+        (node)->next->prev = (node)->prev;\
+        (node)->prev->next = (node)->next;\
+    } while (0)
+
+#define MZX_LIST_REMOVE(node) \
+    MZX_LIST_REMOVE_UNINIT(node); \
+    MZX_INIT_LIST_HEAD(node)
+
+#define MZX_LIST_FOREACH(it, head) \
+    for (decltype(head) it = (head)->next; it != (head); it = it->next) 
+
+#define MZX_LIST_FOREACH_REVERSE(it, head) \
+    for (decltype(head) it = (head)->prev; it != (head); it = it->prev) 
 
 }
 
---
+++ test/test_list.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <mzx/list.h>
+
+struct Test
+{
+    int a;
+    mzx::ListHead list;
+};
+
+int main(int argc, char *argv[])
+{
+    mzx::ListHead test_list;
+    MZX_INIT_LIST_HEAD(&test_list);
+    Test a1{.a=1};
+    MZX_LIST_PUSH_BACK(&a1.list, &test_list);
+    Test a2{.a=2};
+    MZX_LIST_PUSH_FRONT(&a2.list, &test_list);
+    Test a3{.a=3};
+    MZX_LIST_PUSH_BACK(&a3.list, &test_list);
+    MZX_LIST_FOREACH(iter, &test_list)
+    {
+        auto *entry = MZX_LIST_ENTRY(iter, Test, list);
+        std::cout << entry->a << ",";
+    }
+    std::cout << std::endl;
+    MZX_LIST_FOREACH_REVERSE(iter, &test_list)
+    {
+        auto *entry = MZX_LIST_ENTRY(iter, Test, list);
+        std::cout << entry->a << ",";
+    }
+    std::cout << std::endl;
+    return 0;
+}
