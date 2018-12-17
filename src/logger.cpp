b7e846f96a657d25434e1fffec15361c38256064
mzxdream <mzxdream@gmail.com>
2018-12-17 16:39:49 +0800

    log modify

--- src/logger.cpp
+++ src/logger.cpp
@@ -8,9 +8,10 @@ namespace mzx {
 
 static void LogPrint(Logger::Level level, const char *file_name, int line, const char *func_name, const std::string &message)
 {
-    const static char *level_names[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
-    fprintf(stderr, "[mzx %s %s:%d %s] %s\n"
-        , level_names[EnumToValue(level)], file_name, line, func_name, message.c_str());
+    const static char *level_names[] = {"DEBUG", "INFO ", "WARN ", "ERROR", "FATAL"};
+    const static char *level_colors[] = {"\e[32m", "\e[1;37m", "\e[1;33m", "\e[31m", "\e[31m"};
+    fprintf(stderr, "%s[mzx %s %s:%d %s] %s\n\e[0m"
+        , level_colors[EnumToValue(level)], level_names[EnumToValue(level)], file_name, line, func_name, message.c_str());
     fflush(stderr);
 }
 
--- test/test_log.cpp
+++ test/test_log.cpp
@@ -4,5 +4,17 @@
 int main(int argc, char *argv[])
 {
     int a = 2;
-    MZX_CHECK(a != 2);
+    MZX_CHECK(a == 2);
+    MZX_DEBUG("12312312", "  dffdfd");
+    MZX_WARN("ggggg ", "  dfdf", 123, "44");
+    MZX_ERR("ffff ", "  ", " pp");
+    mzx::Logger::SetLevel(mzx::Logger::Level::Debug);
+    MZX_DEBUG("test debug:", 123123123);
+    std::cout << "==================" << std::endl;
+    MZX_INFO("test info:", 44444, 5555, 6666);
+    std::cout << "==================" << std::endl;
+    MZX_WARN("test warn:", "tt", 123, "44");
+    std::cout << "==================" << std::endl;
+    MZX_CHECK(a != 2, "ffdfdfdfdfdf");
+    std::cout << "==================" << std::endl;
 }
