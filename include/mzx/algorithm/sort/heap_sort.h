e3108c1efda5d5d139ecab57d35a86ee461f9ae3
mzxdream <mzxdream@gmail.com>
2019-05-05 15:03:39 +0800

    add heap sort

--- include/mzx/algorithm/sort/heap_sort.h
+++ include/mzx/algorithm/sort/heap_sort.h
@@ -5,9 +5,25 @@
 
 namespace mzx {
 
-template <typename RandIt, typename Compare>
-void MakeHeap(RandIt begin, RandIt end, Compare comp)
+template <typename RandIt, typename  Compare, typename Index>
+void __AdjustHeap(RandIt begin, RandIt end, Index idx, Compare comp)
 {
+    auto swap_idx = idx;
+    auto child_idx = (idx << 1) + 1;
+    if (child_idx < end - begin && comp(*(begin + swap_idx), *(begin + child_idx)))
+    {
+        swap_idx = child_idx;
+    }
+    child_idx = (idx << 1) + 2;
+    if (child_idx < end - begin && comp(*(begin + swap_idx), *(begin + child_idx)))
+    {
+        swap_idx = child_idx;
+    }
+    if (swap_idx != idx)
+    {
+        std::iter_swap(begin + swap_idx, begin + idx);
+        __AdjustHeap(begin, end, swap_idx, comp);
+    }
 }
 
 template <typename RandIt, typename Compare>
@@ -16,6 +32,16 @@ void HeapSort(RandIt begin, RandIt end, Compare comp)
     MZX_CHECK_STATIC((std::is_same<std::random_access_iterator_tag
             , typename std::iterator_traits<RandIt>::iterator_category>::value));
     MZX_CHECK(end > begin);
+    auto len = end - begin;
+    for (auto i = (len - 1) >> 1; i >= 0; --i)
+    {
+        __AdjustHeap(begin, end, i, comp);
+    }
+    for (auto iter = end - 1; iter != begin; --iter)
+    {
+        std::iter_swap(iter, begin);
+        __AdjustHeap(begin, iter, 0, comp);
+    }
 }
 
 template <typename RandIt>
