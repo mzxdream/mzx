ba4614fe771eb10e9b5fac0bbae37edad1b78056
mzxdream <mzxdream@gmail.com>
2019-05-21 21:53:35 +0800

    1

--- test/system/test_sys_util.cpp
+++ test/system/test_sys_util.cpp
@@ -3,6 +3,33 @@
 
 #include <mzx/system/sys_util.h>
 
+void Test3()
+{
+    std::cout << 2 << std::endl;
+    mzx::ForeachStackTrace([](const char *symbol) {
+        std::cout << "symbol:" << symbol << std::endl;
+        return true;
+    });
+}
+
+void Test2()
+{
+    std::cout << 2 << std::endl;
+    Test3();
+}
+
+void Test1()
+{
+    std::cout << 1 << std::endl;
+    Test2();
+}
+
+void Test()
+{
+    std::cout << 0 << std::endl;
+    Test1();
+}
+
 int main(int argc, char *argv[])
 {
     int a = 0;
@@ -10,9 +37,6 @@ int main(int argc, char *argv[])
     std::cout << "a: " << typeid(a).name() << " -> " << mzx::Demangle(typeid(a).name()) << std::endl;
     std::cout << "b: " << typeid(b).name() << " -> " << mzx::Demangle(typeid(b).name()) << std::endl;
     std::cout << "CPU count:" << mzx::GetCPUCount() << std::endl;
-    mzx::ForeachStackTrace([](const char *symbol) {
-        std::cout << "symbol:" << symbol << std::endl;
-        return true;
-    });
+    Test();
     return 0;
 }
