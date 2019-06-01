be82bea50398d0595e8e317bda5a728084d68a9c
mzxdream <mzxdream@gmail.com>
2019-06-01 15:15:59 +0800

    modify logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -3,18 +3,21 @@
 #include <utility>
 
 #include <mzx/logger.h>
-#include <mzx/util.h>
 #include <mzx/system/sys_util.h>
+#include <mzx/util.h>
 
 namespace mzx
 {
 
-static void LogPrint(Logger::Level level, const char *file_name, int line, const char *func_name,
-                     const std::string &message)
+static void LogPrint(Logger::Level level, const char *file_name, int line,
+                     const char *func_name, const std::string &message)
 {
-    const static char *level_names[] = {"DEBUG", "INFO ", "WARN ", "ERROR", "FATAL"};
-    const static char *level_colors[] = {"\e[32m", "\e[1;37m", "\e[1;33m", "\e[31m", "\e[31m"};
-    fprintf(stderr, "%s[mzx %s %s:%d %s] %s\n\e[0m", level_colors[EnumToValue(level)], level_names[EnumToValue(level)],
+    const static char *level_names[] = {"DEBUG", "INFO ", "WARN ", "ERROR",
+                                        "FATAL"};
+    const static char *level_colors[] = {"\e[32m", "\e[1;37m", "\e[1;33m",
+                                         "\e[31m", "\e[31m"};
+    fprintf(stderr, "%s[mzx %s %s:%d %s] %s\n\e[0m",
+            level_colors[EnumToValue(level)], level_names[EnumToValue(level)],
             file_name, line, func_name, message.c_str());
     fflush(stderr);
 }
