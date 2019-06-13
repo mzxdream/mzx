52707f0f4196c823f0a4e0b8deb586069cc37e1c
mzxdream <mzxdream@gmail.com>
2019-06-13 16:52:46 +0800

    change singleton to c++11

--- include/mzx/singleton.h
+++ include/mzx/singleton.h
@@ -3,12 +3,10 @@
 
 #include <mutex>
 
-#include <mzx/null_mutex.h>
-
 namespace mzx
 {
 
-template <typename T, typename Mutex = NullMutex>
+template <typename T>
 class Singleton
 {
 protected:
@@ -20,69 +18,10 @@ protected:
 public:
     static T &Instance()
     {
-        return instance_.Instance();
+        static T instance;
+        return instance;
     }
-    static void DestroyInstance()
-    {
-        return instance_.DestoryInstance();
-    }
-
-private:
-    class SingletonBase
-    {
-    public:
-        SingletonBase()
-            : instance_(nullptr)
-        {
-        }
-        ~SingletonBase()
-        {
-            if (instance_)
-            {
-                delete instance_;
-                instance_ = nullptr;
-            }
-        }
-        SingletonBase(const SingletonBase &) = delete;
-        SingletonBase &operator=(const Singleton &) = delete;
-
-    public:
-        T &Instance()
-        {
-            if (!instance_)
-            {
-                std::lock_guard<Mutex> lock(mtx_);
-                if (!instance_)
-                {
-                    instance_ = new T();
-                }
-            }
-            return *instance_;
-        }
-        void DestroyInstance()
-        {
-            if (instance_)
-            {
-                std::lock_guard<Mutex> lock(mtx_);
-                if (instance_)
-                {
-                    delete instance_;
-                    instance_ = nullptr;
-                }
-            }
-        }
-
-    private:
-        T *instance_;
-        Mutex mtx_;
-    };
-
-private:
-    static SingletonBase instance_;
 };
-
-template <typename T, typename Mutex>
-typename Singleton<T, Mutex>::SingletonBase Singleton<T, Mutex>::instance_;
 
 } // namespace mzx
 
