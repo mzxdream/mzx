ccedf960f20fca62f13f23f4e541ce877003042b
mzxdream <mzxdream@gmail.com>
2019-06-19 11:20:53 +0800

    add pair hash

--- include/mzx/util.h
+++ include/mzx/util.h
@@ -13,6 +13,16 @@ inline typename std::underlying_type<E>::type EnumToValue(E e)
     return static_cast<typename std::underlying_type<E>::type>(e);
 }
 
+struct PairHash
+{
+public:
+    template <typename T, typename U>
+    std::size_t operator()(const std::pair<T, U> &x) const
+    {
+        return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+    }
+};
+
 } // namespace mzx
 
 #endif
