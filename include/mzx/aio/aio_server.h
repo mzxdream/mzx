8b7adb303f9648ecd9486f9c514372e0e59af35d
mzxdream <mzxdream@gmail.com>
2019-06-29 22:58:46 +0800

    aio server add wakeup

--- include/mzx/aio/aio_server.h
+++ include/mzx/aio/aio_server.h
@@ -5,6 +5,7 @@
 #include <list>
 #include <mutex>
 
+#include <mzx/aio/aio_handler.h>
 #include <mzx/thread.h>
 
 namespace mzx
@@ -20,17 +21,24 @@ public:
     AIOServer(const AIOServer &) = delete;
     AIOServer &operator=(const AIOServer &) = delete;
 
+    void Stop();
     void Exec(ExecFunc func);
     void Post(ExecFunc func);
 
 private:
+    void Wakeup();
+    void OnWakeup();
+    void HandleExecQueue();
+
     virtual void _Run() override;
 
 private:
     int epoll_fd_{-1};
     int wakeup_fd_{-1};
-    std::mutex exec_queue_mtx;
+    AIOHandler *wakeup_handler_{nullptr};
+    std::mutex exec_queue_mtx_;
     std::list<ExecFunc> exec_queue_;
+    volatile bool stop_flag_{false};
 };
 
 } // namespace mzx
--- src/aio/aio_server.cpp
+++ src/aio/aio_server.cpp
@@ -1,5 +1,6 @@
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
+#include <unistd.h>
 
 #include <mzx/aio/aio_server.h>
 #include <mzx/logger.h>
@@ -13,18 +14,72 @@ AIOServer::AIOServer()
     MZX_CHECK(epoll_fd_ >= 0);
     wakeup_fd_ = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
     MZX_CHECK(wakeup_fd_ >= 0);
+    wakeup_handler_ = new AIOHandler(wakeup_fd_, epoll_fd_);
+    wakeup_handler_->EnableRead();
 }
 
 AIOServer::~AIOServer()
 {
+    Stop();
+    if (wakeup_handler_)
+    {
+        delete wakeup_handler_;
+    }
+    close(wakeup_fd_);
+    close(epoll_fd_);
+}
+
+void AIOServer::Stop()
+{
+    stop_flag_ = true;
+    Wakeup();
+    Join();
 }
 
 void AIOServer::Exec(ExecFunc func)
 {
+    MZX_CHECK(func != nullptr);
+    if (IsCurrentThread())
+    {
+        func();
+    }
+    else
+    {
+        Post(std::move(func));
+    }
 }
 
 void AIOServer::Post(ExecFunc func)
 {
+    std::lock_guard<std::mutex> lock(exec_queue_mtx_);
+    if (exec_queue_.empty())
+    {
+        Wakeup();
+    }
+    exec_queue_.push_back(func);
+}
+
+void AIOServer::Wakeup()
+{
+    uint64_t tmp = 1;
+    write(wakeup_fd_, &tmp, sizeof(tmp));
+}
+
+void AIOServer::OnWakeup()
+{
+    uint64_t tmp = 0;
+    read(wakeup_fd_, &tmp, sizeof(tmp));
+}
+
+void AIOServer::HandleExecQueue()
+{
+    MZX_CHECK(IsCurrentThread());
+    while (!exec_queue_.empty())
+    {
+        auto exec_func = std::move(exec_queue_.front());
+        exec_queue_.pop_front();
+        exec_func();
+    }
 }
 
 void AIOServer::_Run()
