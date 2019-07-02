ae6fccc12557dcfbb7523fa432c67dd960db79a8
mzxdream <mzxdream@gmail.com>
2019-07-02 21:59:34 +0800

    modify test

--- test/build.sh
+++ test/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
     echo "require cpp"
     exit 0
 fi
 
-g++ -Wall -g -rdynamic -o test $1 -I../include -L../lib -l:libmzx.a -lpthread -std=c++11
+out=test
+
+if [ $# -ge 2 ]; then
+    out=$2
+fi
+
+g++ -Wall -g -rdynamic -o $out.out $1 -I../include -L../lib -l:libmzx.a -lpthread -std=c++11
